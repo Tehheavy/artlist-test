373e509fddac95867091fed2c9b9328cd658ce23
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-11-08 01:05:21 +0200

    Updates

---
+++ precache-manifest.b31257656579c8f2035b576a93cf15bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9022232a6e5e0ec927281ba0e7c7f85a",
+    "url": "/artlist-test/index.html"
+  },
+  {
+    "revision": "9660b50cbd02d636ea24",
+    "url": "/artlist-test/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "088e38b1c49ffd21ae51",
+    "url": "/artlist-test/static/css/main.39a871fd.chunk.css"
+  },
+  {
+    "revision": "9660b50cbd02d636ea24",
+    "url": "/artlist-test/static/js/2.d14735d0.chunk.js"
+  },
+  {
+    "revision": "088e38b1c49ffd21ae51",
+    "url": "/artlist-test/static/js/main.98717f12.chunk.js"
+  },
+  {
+    "revision": "e1832f9125bd53c41675",
+    "url": "/artlist-test/static/js/runtime-main.79741a2f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/artlist-test/static/media/logo.25bf045c.svg"
+  }
+]);
