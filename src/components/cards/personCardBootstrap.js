c20be4ec01b237580c232a0cfa090382c2e18fb4
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-11-08 23:26:43 +0200

    Update personCardBootstrap.js

--- src/components/cards/personCardBootstrap.js
+++ src/components/cards/personCardBootstrap.js
@@ -9,14 +9,14 @@ function personCardBootstrap(props) {
   console.log(url);
   return (
     <div className="person-card-bootstrap-class">
-      <Card className="bg-dark text-white">
+      <Card className="bg-transparent text-white">
         <Card.Img src={props.imageUrl} alt="Card image" />
         <Card.ImgOverlay>
           <div className="person-card-information-bootstrap-class">
-              <p>{props.occupation}</p>
+              <p style={{fontWeight:"900"}}>{props.occupation}</p>
                 <div>
-                  <p style={{color:"yellow",marginRight:"1rem"}}>{props.name}</p>
-                  <p>{props.jobTitle}</p>
+                  <p style={{color:"yellow",marginRight:"1rem",fontWeight:"900"}}>{props.name}</p>
+                  <p style={{fontWeight:"900"}}>{props.jobTitle}</p>
                 </div>
           </div>
 
